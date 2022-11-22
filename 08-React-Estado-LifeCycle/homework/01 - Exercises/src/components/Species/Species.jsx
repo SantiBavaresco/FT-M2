a29fd82433f3c31d8ea424e0ce79925ef983672c
Santiago Bavaresco <santiago.bavaresco@codere.com>
2022-11-22 16:33:32 -0300

    08-React-Estado-LifeCycle

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function Species () {
+export default function Species ({ species, handleSpecies, handleAllSpecies }) {
   return (
     <div>
-
+      
+      <h2>Species</h2> {/* --- 4.1 --- */}
+      {/* --- 4.2 --- */}
+      {species.map((specie) => (
+        <button key={specie} onClick={handleSpecies} value={specie}>{/* --- 4.4 --- */}
+          {specie} {/* --- 4.3 --- */}
+        </button>
+      ))}
+      {/* --- 4.5 --- */}
+      <button onClick={handleAllSpecies}>All Animals</button>
     </div>
   )
 }
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -7,9 +7,69 @@ import './Zoo.module.css';
 
 export default function Zoo() {
    /* Escribe acá tu código */
+   // ------------- 1.1 -------------
+   const [zoo, setZoo] = React.useState({
+      zooName: '',
+      animals: [],
+      species: [],
+      allAnimals: [],
+   });
+   // ------------- 1.6 -------------
+   const handleInputChange = (evento) => {
+      // console.log(evento.target.value);
+      // ------------- 1.7 -------------
+      setZoo({
+        ...zoo,
+        zooName: evento.target.value,
+      });
+    };
+  // ------------- 2.3 -------------
+    const handleSpecies = (e) => {
+      setZoo({
+        ...zoo,
+        // ------------- 5.2 -------------
+        animals: zoo.allAnimals.filter( 
+          (animal) => animal.specie === e.target.value
+        ),
+      });
+    };
+  // ------------- 2.4 -------------
+    const handleAllSpecies = () => {
+      setZoo({
+        ...zoo,
+        // ------------- 5.3 -------------
+        animals: zoo.allAnimals,
+      });
+    };
+   // ------------- 2.2 -------------
+    React.useEffect(() => {
+      fetch("http://localhost:3001/zoo")
+        .then((res) => res.json())
+        .then((data) =>
+          setZoo({
+            ...zoo,
+            animals: data.animals,
+            species: data.species,
+            allAnimals: data.animals,
+          })
+        )
+        .catch((error) => console.log(error));
+  
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []); 
+//-------------------------------------------------------------------------------------------
    return (
       <div>
-         <h1>-</h1>
+         <label>Zoo Name:</label>   {/* --- 1.2 --- */}
+         <input onChange={handleInputChange} value={zoo.zooName} />  {/* --- 1.3, 1.5, 1.8 --- */}
+         <h1>{zoo.zooName}</h1>  {/* --- 1.4 --- */}
+            {/* --- 2.5 --- */}
+            <Species
+            species={zoo.species}
+            handleSpecies={handleSpecies}
+            handleAllSpecies={handleAllSpecies}
+            />
+            <Animals animals={zoo.animals} />
       </div>
    );
 }
