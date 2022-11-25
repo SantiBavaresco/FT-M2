de4dacf8737e425889415d9ceda91cdac24b64ee
Santiago Bavaresco <santiago.bavaresco@codere.com>
2022-11-25 13:43:11 -0300

    09-React-Routing

--- 09-React-Routing/homework/01 - Exercises/src/App.js
+++ 09-React-Routing/homework/01 - Exercises/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
 /* eslint-disable */
 import Home from "./components/Home/Home.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
@@ -7,6 +8,19 @@ import Promotions from "./components/Promotions/Promotions.jsx";
 import CardDetail from "./components/CardDetail/CardDetail.jsx";
 /* eslint-disable */
 
+// Importa Routes y Route desde react-router-dom
+//  y renderiza el componente Routes.
+
 export default function App() {
-  return <div></div>;
+  return (
+  <div>
+    <NavBar/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/shipping" element={ <Shipping/> } />
+      <Route path="/promotions" element={ <Promotions/> } />
+      <Route path="/cruises/:id" element={ <CardDetail/> } /> 
+            {/* :id es una variable*/}
+    </Routes>
+  </div>)
 }
